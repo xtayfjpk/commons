a4b45667468371f082347b118a5f267edc511f64
zhangjin <zhangjin@comba.com.cn>
2016-07-25 10:46:04 +0800

    添加 ant.jar中zip压缩相关类

---
+++ src/main/java/com/nightsoul/commons/ZipUtils.java
@@ -0,0 +1,58 @@
+package com.nightsoul.commons;
+
+import java.io.File;
+
+public abstract class ZipUtils {
+	private ZipUtils(){}
+	
+	public static void zipDirectory(String srcDirectory) {
+		
+	}
+
+	public static void zipDirectory(File srcDirectory) {
+		
+	}
+
+	public static void zipDirectory(String srcDirectory, String destZipFilepath) {
+		
+	}
+
+	public static void zipDirectory(File srcDirectory, File destZipFile) {
+		
+	}
+
+	public static void zipDirectoryFiles(String srcDirectory) {
+		
+	}
+
+	public static void zipDirectoryFiles(File srcDirectory) {
+		
+	}
+
+	public static void zipDirectoryFiles(String srcDirectory, String destZipFilepath) {
+		
+	}
+
+	public static void zipDirectoryFiles(File srcDirectory, File destZipFile) {
+		
+	}
+
+
+	public static void unzip(File zipFile) {
+		
+	}
+
+	public static void unzip(File zipFile, File destDirectory) {
+		
+	}
+
+	public static void unzip(String zipFilepath) {
+		
+	}
+
+	public static void unzip(String zipFilepath, String destDirectory) {
+		
+	}
+	
+	
+}
