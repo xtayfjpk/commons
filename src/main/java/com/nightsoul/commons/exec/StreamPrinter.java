83e4bade1d5690eeb16439462d0a86e110218753
zhangjin <zhangjin@kingyea.com.cn>
2016-09-11 18:12:46 +0800

    添加工具类

---
+++ src/main/java/com/nightsoul/commons/exec/StreamPrinter.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Robert "Unlogic" Olofsson (unlogic@unlogic.se).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+ ******************************************************************************/
+package com.nightsoul.commons.exec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import org.apache.commons.io.IOUtils;
+
+import com.nightsoul.commons.util.ObjectUtils;
+
+
+/**
+ * A simple buffered {@link StreamHandler} implementation that prints the input from the {@link InputStream} to the given {@link OutputStream}. If no {@link OutputStream} is given it defaults to System.out.<p>
+ * 
+ * This implementation inputStream based on the {@link PrintWriter}, {@link InputStreamReader} and {@link BufferedReader} classes.
+ * 
+ * @author Robert "Unlogic" Olofsson (unlogic@unlogic.se)
+ *
+ */
+public class StreamPrinter extends Thread implements StreamHandler {
+	private InputStream inputStream;
+	private String prefix;
+	private OutputStream outputStream;
+
+	private boolean terminated = false;
+	
+	public StreamPrinter(String prefix, OutputStream os) {
+		super();
+		this.outputStream = os;
+		this.prefix = prefix;
+	}
+	
+	public StreamPrinter(OutputStream os) {
+		super();
+		this.outputStream = os;
+	}
+
+	public StreamPrinter() {}
+
+	public boolean isTerminated(){
+		
+		return terminated;
+	}
+	
+	public void terminate(){
+		
+		this.terminated = true;
+	}
+	
+	@Override
+	public void run() {
+		InputStreamReader inputStreamReader = null;
+		BufferedReader bufferedReader = null;
+		try {
+			PrintWriter pw = null;
+			if (outputStream != null) {
+				pw = new PrintWriter(outputStream);
+			} else {
+				pw = new PrintWriter(System.out);
+			}
+
+			inputStreamReader = new InputStreamReader(inputStream);
+			bufferedReader = new BufferedReader(inputStreamReader);
+
+			String line = null;
+			if(ObjectUtils.isNotNull(prefix)){
+				while (!terminated && ObjectUtils.isNotNull((line = bufferedReader.readLine()))) {
+					pw.println(prefix + line);
+				}
+			} else {
+				while (!terminated && ObjectUtils.isNotNull((line = bufferedReader.readLine()))) {
+					pw.println(line);
+				}
+			}
+
+			if(ObjectUtils.isNotNull(pw)) {
+				pw.flush();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally{
+			IOUtils.closeQuietly(bufferedReader);
+			IOUtils.closeQuietly(inputStreamReader);
+			this.terminated = true;
+		}
+	}
+
+	public void handleStream(InputStream is) {
+		this.inputStream = is;
+		this.start();
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public OutputStream getOutputStream() {
+		return outputStream;
+	}
+
+	public boolean isFinished() {
+		return !this.isAlive();
+	}
+
+	public void awaitFinish() {
+		try {
+			this.join();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
